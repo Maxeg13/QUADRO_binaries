e0a0644a508f36f2b3fc731a6f5ad36920617d17
Maxeg13 <chibisaincrada@gmail.com>
2017-10-04 16:31:21 +0400

    nice

--- release/moc_mainwindow.cpp
+++ release/moc_mainwindow.cpp
@@ -19,8 +19,8 @@
 
 QT_BEGIN_MOC_NAMESPACE
 struct qt_meta_stringdata_MainWindow_t {
-    QByteArrayData data[12];
-    char stringdata0[115];
+    QByteArrayData data[13];
+    char stringdata0[128];
 };
 #define QT_MOC_LITERAL(idx, ofs, len) \
     Q_STATIC_BYTE_ARRAY_DATA_HEADER_INITIALIZER_WITH_OFFSET(len, \
@@ -40,12 +40,14 @@ QT_MOC_LITERAL(7, 69, 9), // "reconnect"
 QT_MOC_LITERAL(8, 79, 6), // "getEMG"
 QT_MOC_LITERAL(9, 86, 7), // "kickMyo"
 QT_MOC_LITERAL(10, 94, 6), // "getCor"
-QT_MOC_LITERAL(11, 101, 13) // "buttonClicked"
+QT_MOC_LITERAL(11, 101, 13), // "buttonClicked"
+QT_MOC_LITERAL(12, 115, 12) // "serialChoose"
 
     },
     "MainWindow\0featureOutSignal\0\0vector<float>\0"
     "sended\0drawing\0getFeature\0reconnect\0"
-    "getEMG\0kickMyo\0getCor\0buttonClicked"
+    "getEMG\0kickMyo\0getCor\0buttonClicked\0"
+    "serialChoose"
 };
 #undef QT_MOC_LITERAL
 
@@ -55,7 +57,7 @@ static const uint qt_meta_data_MainWindow[] = {
        7,       // revision
        0,       // classname
        0,    0, // classinfo
-       9,   14, // methods
+      10,   14, // methods
        0,    0, // properties
        0,    0, // enums/sets
        0,    0, // constructors
@@ -63,17 +65,18 @@ static const uint qt_meta_data_MainWindow[] = {
        2,       // signalCount
 
  // signals: name, argc, parameters, tag, flags
-       1,    1,   59,    2, 0x06 /* Public */,
-       4,    0,   62,    2, 0x06 /* Public */,
+       1,    1,   64,    2, 0x06 /* Public */,
+       4,    0,   67,    2, 0x06 /* Public */,
 
  // slots: name, argc, parameters, tag, flags
-       5,    0,   63,    2, 0x0a /* Public */,
-       6,    1,   64,    2, 0x0a /* Public */,
-       7,    1,   67,    2, 0x0a /* Public */,
-       8,    1,   70,    2, 0x0a /* Public */,
-       9,    0,   73,    2, 0x0a /* Public */,
-      10,    0,   74,    2, 0x0a /* Public */,
-      11,    1,   75,    2, 0x0a /* Public */,
+       5,    0,   68,    2, 0x0a /* Public */,
+       6,    1,   69,    2, 0x0a /* Public */,
+       7,    1,   72,    2, 0x0a /* Public */,
+       8,    1,   75,    2, 0x0a /* Public */,
+       9,    0,   78,    2, 0x0a /* Public */,
+      10,    0,   79,    2, 0x0a /* Public */,
+      11,    1,   80,    2, 0x0a /* Public */,
+      12,    0,   83,    2, 0x0a /* Public */,
 
  // signals: parameters
     QMetaType::Void, 0x80000000 | 3,    2,
@@ -87,6 +90,7 @@ static const uint qt_meta_data_MainWindow[] = {
     QMetaType::Void,
     QMetaType::Void,
     QMetaType::Void, QMetaType::Int,    2,
+    QMetaType::Void,
 
        0        // eod
 };
@@ -106,6 +110,7 @@ void MainWindow::qt_static_metacall(QObject *_o, QMetaObject::Call _c, int _id, 
         case 6: _t->kickMyo(); break;
         case 7: _t->getCor(); break;
         case 8: _t->buttonClicked((*reinterpret_cast< int(*)>(_a[1]))); break;
+        case 9: _t->serialChoose(); break;
         default: ;
         }
     } else if (_c == QMetaObject::IndexOfMethod) {
@@ -151,13 +156,13 @@ int MainWindow::qt_metacall(QMetaObject::Call _c, int _id, void **_a)
     if (_id < 0)
         return _id;
     if (_c == QMetaObject::InvokeMetaMethod) {
-        if (_id < 9)
+        if (_id < 10)
             qt_static_metacall(this, _c, _id, _a);
-        _id -= 9;
+        _id -= 10;
     } else if (_c == QMetaObject::RegisterMethodArgumentMetaType) {
-        if (_id < 9)
+        if (_id < 10)
             *reinterpret_cast<int*>(_a[0]) = -1;
-        _id -= 9;
+        _id -= 10;
     }
     return _id;
 }
